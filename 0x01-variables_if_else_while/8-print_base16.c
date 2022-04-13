bef8d25dd4aa79f5db47677e3327f92501e2dfa2
moclackobi <marvelobi@ymail.com>
2022-04-13 06:43:05 -0700

    task 8fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 {
 char x;
 
-for(x = '0'; x <= '9'; ++x)
+for (x = '0'; x <= '9'; ++x)
 putchar(x);
-for(x = 'a'; x <= 'f'; ++x)
+for (x = 'a'; x <= 'f'; ++x)
 putchar(x);
 
 putchar('\n');
-		
+
 return (0);
 }
