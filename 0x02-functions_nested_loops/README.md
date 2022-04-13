743fb02ee7901d6e28e1f6cbf67775aadccb7633
moclackobi <marvelobi@ymail.com>
2022-04-13 14:08:28 -0700

    task 1

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,7 @@
+# C - Functions, nestedloops
+
+## Header file :file_folder:
+* [main.h](./main.h): Header file containing all prototypes for all 
+functions written in the project.
+
+This programmes teaches about nested loop.
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet _%10(void);
+int _inlower(int c);
+int _inalpha(int c);
+int print_sign(int n);
+int _abc(int);
+int print_last_digit(int);
+void jack_baver(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif /* _MAIN_H_ */
