d058ebfe4f3a82c474a7a0b728deba805f340ad3
moclackobi <marvelobi@ymail.com>
2022-04-15 06:50:22 -0700

    task 7fixed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - computes the absoloute value of an integer.
+ * _abs -Computes the absoloute value of an integer.
  * @n: the integer to be computed.
  *
  * Return: The absolute value of the integer.
