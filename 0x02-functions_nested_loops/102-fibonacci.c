879ddd62bbeb80d2f1903c95ac0e030f481649b1
moclackobi <marvelobi@ymail.com>
2022-04-17 02:08:04 -0700

    task 103

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,13 +16,13 @@ for (count = 0; count < 50; count++)
 sum = fib1 + fib2;
 printf("%lu", sum);
 
-fib1 =fib2:
+fib1 =fib2;
 fib2 = sum;
 
 if (count == 49)
 printf("\n");
 else 
-printf(',');
+printf(', ');
 }
 
 return (0);
