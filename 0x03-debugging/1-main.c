5f99c38cc998a35ec33573fd606f2ae4d87d697f
moclackobi <marvelobi@ymail.com>
2022-04-17 23:00:58 -0700

    task 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,7 @@
-# include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 
@@ -14,13 +13,12 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 
-/*
-*while (i < 10)
-*{
-*infinite loop - no increase no varaible
-*putchar(i);
-*}
-*/
-printf("infinite loop avoided! \\o/\");
+ /*
+  * while (i > 10)
+  * {
+  * putchar(i);
+  * }
+  */
+printf("infinite loop avoided! \\o/\n");
 return (0);
 }
