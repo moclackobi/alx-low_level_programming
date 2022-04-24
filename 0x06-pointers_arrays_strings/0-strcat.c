6b684ecacd7139afd3484171bdca47312cfe6d88
moclackobi <marvelobi@ymail.com>
2022-04-23 19:28:04 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * *_strcat - concatenates two strings
  * @dest: second string to be concatenated to src
@@ -9,6 +8,7 @@
 char *_strcat(char *dest, char *src)
 {
 int x, y;
+
 for (x = 0; dest[x] != '\0'; x++)
 ;
 for (y = 0; src[y] != '\0'; y++)
