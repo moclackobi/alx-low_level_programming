3e25ad0390565a48f7c1b2708898bd1c8a5945b6
moclackobi <marvelobi@ymail.com>
2022-04-24 06:59:49 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,8 @@ char *_strncat(char *dest, char *src, int n)
 int x, y;
 
 for (x = 0; dest[x] != '\0'; x++)
-;
-for (y = 0; src[y] != '\0' && n > 0; y++, n++, x++)
+        ;
+for (y = 0; src[y] != '\0' && n > 0; y++, n--, x++)
 {
 dest[x] = src[y];
 }
