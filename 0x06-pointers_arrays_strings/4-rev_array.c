530647b3313fc3508698e7c2a202bca0a382d719
moclackobi <marvelobi@ymail.com>
2022-04-24 04:02:20 -0700

    taks 5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverses content of an array of integer
+ * @a: array of integer
+ * @n: number of element in array
+ * Return: Always 0 (success)
+ */
+void reverse_array(int *a, int n)
+{
+int x, y;
+
+for (x = 0; x < (n / 2); x++)
+{
+y = a[x];
+a[x] = a[n - x - 1];
+a[a - x -1] = y;
+}
+}
